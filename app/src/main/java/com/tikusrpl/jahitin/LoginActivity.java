3a669f26e585260497d3f54243a3349b32ccaa5e
Refa Annisa <rans5021@gmail.com>
2019-12-03 23:26:18 +0700

    Connect db & login java

--- app/src/main/java/com/tikusrpl/jahitin/LoginActivity.java
+++ app/src/main/java/com/tikusrpl/jahitin/LoginActivity.java
@@ -1,14 +1,74 @@
 package com.tikusrpl.jahitin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private Button button;
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.login_password);
+        button = findViewById(R.id.login_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("") || password.getText().toString().equals("")){
+                    Toast.makeText(LoginActivity.this, "Complete your login form!", Toast.LENGTH_SHORT).show();
+                } else{
+                    String id = username.getText().toString();
+                    String pw = password.getText().toString();
+
+                    switch (id){
+                        case "refaans":
+                            checkLogin(id, pw);
+                            break;
+                        case "illiyya":
+                            break;
+
+                    }
+
+                }
+            }
+        });
+    }
+
+    private void checkLogin(String id, String pw) {
+        dbRef = db.getReference().child("account").child(id);
+
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("success",value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("error", databaseError.getMessage());
+            }
+        });
     }
 }
